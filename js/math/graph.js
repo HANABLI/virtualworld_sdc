2e03931fcdfd461dbaad02acea95cb4fffb28e80
Hatem Nabli <hatem.nabli@actemium.com>
2024-01-28 19:36:31 +0100

    feat: creation of several fonctions to randomly add or remove points and segments

--- js/math/graph.js
+++ js/math/graph.js
@@ -13,4 +13,57 @@ class Graph {
             point.draw(ctx);
         }
     }
+
+    dispose() {
+        this.points.length = 0;
+        this.segments.length = 0;
+    }
+
+    addPoint(point) {
+        this.points.push(point);
+    }
+
+    addSegment(segment) {
+        this.segments.push(segment);
+    }
+
+    removeSegments(segs) {
+        segs.forEach(seg => {
+            this.segments.splice(this.segments.indexOf(seg), 1);
+        });     
+    }
+
+    removePoint(point) {
+        const segments = this.getSegmentsWithPoint(point);
+        this.removeSegments(segments);
+        this.points.splice(this.points.indexOf(point), 1);
+    }
+
+    getSegmentsWithPoint(point) {
+        return this.segments.filter(segment => segment.contains(point));
+    }
+
+    isContainsPoint(point) {
+        return this.points.find(p => p.equals(point));
+    }
+
+    isContainsSegment(segment) {
+        return this.segments.find(s => s.equals(segment));
+    }
+
+    tryAddPoint(point) {
+        if (!this.isContainsPoint(point)) {
+            this.addPoint(point);
+            return true;
+        }
+        return false;
+    }
+
+    tryAddSegment(segment) {
+        if (!this.isContainsSegment(segment) && !segment.p1.equals(segment.p2)) {
+            this.addSegment(segment);
+            return true;
+        }
+        return false;
+    }
 }
--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -3,7 +3,16 @@ class Segment {
         this.p1 = p1;
         this.p2 = p2;
     }
+ 
+    equals(segmet) {
+        //return (this.p1 == segmet.p1 && this.p2 == segmet.p2) || (this.p2 == segmet.p1 && this.p1 == segmet.p2);
+        return this.contains(segmet.p1) && this.contains(segmet.p2);
+    }
 
+    contains(point) {
+        return this.p1.equals(point) || this.p2.equals(point);
+    }
+    
     draw(ctx, width = 2, color = "black") {
         ctx.beginPath();
         ctx.lineWidh = width;
