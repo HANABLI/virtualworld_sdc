d1d58d4b4672948a0e76a8a2466be51945ac1b4c
Hatem Nabli <hatem.nabli@actemium.com>
2024-01-30 18:22:52 +0100

    feat : EventListeners factorisation        Dash segment creation        ViewPort creation with several pan events such as mouseweel event, mousedown event, mousemove event and mouseup event.

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -1,45 +1,71 @@
 class GraphEditor {
-    constructor(canvas, graph) {
-        this.canvas = canvas;
+    constructor(viewPort, graph) {
+        this.viewPort = viewPort;
+        this.canvas = this.viewPort.canvas;
         this.graph = graph;
         this.ctx = this.canvas.getContext("2d");
         this.selected = null;
         this.hovered = null;
+        this.dragging = false;
+        this.mousePoint = null;
         this.#addEventListeners();
     }
 
+    #removePoint(point) {
+        this.graph.removePoint(point);
+            if (this.selected == point) {
+                this.selected = null;
+            }
+            this.hovered = null;  
+    }
+
     #addEventListeners() {
-        this.canvas.addEventListener("mousedown", (evt) => {
-            if (evt.button == 0) {     // left click           
-                const mousePoint = new Point(evt.offsetX, evt.offsetY);
-                //this.hovered = getNearestPoint(mousePoint, this.graph.points, 10); - not needed (present in mousemove event)
-                if (this.hovered) {
-                    this.selected = this.hovered;
-                    return;
-                }
-                this.graph.addPoint(mousePoint);
-                this.selected = mousePoint;
-                console.log("added point:", mousePoint);  
-                }
-            if (evt.button == 2) { // right click
-                if (this.hovered) {
-                    this.graph.removePoint(this.hovered);
-                    if (this.selected == this.hovered) {
-                        this.selected = null;
-                    }
-                    this.hovered = null;  
-                }          
-            }
-        });
-        this.canvas.addEventListener('mousemove', (evt) => {
-            const mousePoint = new Point(evt.offsetX, evt.offsetY);
-            this.hovered = getNearestPoint(mousePoint, this.graph.points, 10);
-            if (this.dragging == true) {
-                this.selected.x = mousePoint.x;
-                this.selected.y = mousePoint.y;
-            }
-        });
+        this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+        this.canvas.addEventListener('mousemove', this.#handleMouseMove.bind(this));
         this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
+        this.canvas.addEventListener("mouseup", () => this.dragging = false);
+    }
+
+    
+
+    #handleMouseMove(evt) {
+        this.mousePoint = this.viewPort.getMouse(evt, true);
+        this.hovered = getNearestPoint(this.mousePoint, this.graph.points, 10);
+        if (this.dragging == true) {
+            this.selected.x = this.mousePoint.x;
+            this.selected.y = this.mousePoint.y;
+        }
+    }
+
+    #handleMouseDown(evt) {
+        if (evt.button == 0) {     // left click           
+            //const mousePoint = new Point(evt.offsetX, evt.offsetY);
+            //this.hovered = getNearestPoint(mousePoint, this.graph.points, 10); - not needed (present in mousemove event)
+            if (this.hovered) {
+                this.#select(this.hovered);                   
+                this.dragging = true;
+                return;
+            }
+            this.graph.addPoint(this.mousePoint);
+            this.#select(this.mousePoint);
+            this.hovered = this.mousePoint;
+            console.log("added point:", this.mousePoint);  
+            }
+        if (evt.button == 2) { // right click
+
+            if (this.selected) {
+                this.selected = null;
+            } else if (this.hovered) {
+                this.#removePoint(this.hovered);
+            }        
+        }
+    }
+
+    #select(point) {
+        if (this.selected) {
+            this.graph.tryAddSegment(new Segment(this.selected, point));
+        }
+        this.selected = point;
     }
 
     display() {
@@ -48,6 +74,8 @@ class GraphEditor {
             this.hovered.draw(this.ctx, { fill: true});
         }
         if (this.selected) {
+            const intent = this.hovered ? this.hovered : this.mousePoint;
+            new Segment(this.selected, intent).draw(ctx, { dash: [3, 3] });
             this.selected.draw(this.ctx, {outline: true});
         }
     }
---
+++ js/viewport.js
@@ -0,0 +1,79 @@
+class ViewPort {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.zoom = 1; 
+        this.center = new Point(canvas.width / 2, canvas.height / 2);
+        this.offset = scale(this.center, -1);
+        this.pan = {
+            start: new Point(0, 0),
+            end: new Point(0, 0),
+            offset: new Point(0, 0),
+            active: false
+        };       
+        this.#addEventListeners();
+    }
+
+
+    #addEventListeners() {
+        this.canvas.addEventListener("mousewheel", this.#handleMouseWheel.bind(this));
+        this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+        this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
+        this.canvas.addEventListener("mouseup", this.#handleMouseUp.bind(this));
+    }
+
+    #handleMouseDown(evt) {
+        if (evt.button == 1) {
+            this.pan.start = this.getMouse(evt);
+            this.pan.active = true;
+        }
+    }
+
+    #handleMouseUp(evt) {
+        if (this.pan.active) {
+            this.offset = add(this.offset, this.pan.offset);
+            this.pan = {
+                start: new Point(0, 0),
+                end: new Point(0, 0),
+                offset: new Point(0, 0),
+                active: false
+            };
+        }
+    }
+
+    #handleMouseMove(evt) {
+        if (this.pan.active) {
+            this.pan.end = this.getMouse(evt);
+            this.pan.offset = substract(this.pan.end, this.pan.start);
+        }
+    }
+
+    #handleMouseWheel(evt) {
+        const direction = Math.sign(evt.deltaY);
+        const step = 0.1
+        this.zoom += direction * step;
+        this.zoom = Math.max(1, Math.min(5, this.zoom));
+    }
+
+    getMouse(evt, substractPanOffset = false) {
+        const p =  new Point(
+            (evt.offsetX - this.center.x) * this.zoom - this.offset.x,
+            (evt.offsetY - this.center.y) * this.zoom - this.offset.y
+        );
+        return substractPanOffset ? substract(p, this.pan.offset) : p;
+    }
+
+    getOffset() {
+        return add(this.offset, this.pan.offset);
+    }
+
+    reset() {
+        this.ctx.restore();
+        this.ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+        this.ctx.save();
+        this.ctx.translate(viewPort.center.x, viewPort.center.y);
+        this.ctx.scale(1 / viewPort.zoom, 1 / viewPort.zoom);
+        const offset = viewPort.getOffset();
+        this.ctx.translate(offset.x, offset.y);
+    }
+}
