3d518aea6b4e84aa56770879857cb5bf8ba8a0ff
Hatem Nabli <hatem.nabli@actemium.com>
2024-01-31 01:51:08 +0100

    feat : Polygon and envelope creation        envelope class use the polygon shape to create the envelope over a segment

--- js/math/utils.js
+++ js/math/utils.js
@@ -26,3 +26,17 @@ function substract(p1, p2) {
 function scale(p, scaler) {
     return new Point(p.x * scaler, p.y * scaler);
 }
+
+function angle(p) {
+    return Math.atan2(p.y, p.x);
+}
+
+function translate(loc, angle, offset) {
+    return new Point(
+        loc.x + Math.cos(angle) * offset,
+        loc.y + Math.sin(angle) * offset
+    );
+}
+
+
+
